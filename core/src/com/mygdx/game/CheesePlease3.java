964b60a0bba3580a6206341166bf5631a5e77006
Pavel <pavel.baskakov@yandex.ru>
2018-11-14 16:18:10 +0300

    Страница 49 (анимация)

--- core/src/com/mygdx/game/CheesePlease3.java
+++ core/src/com/mygdx/game/CheesePlease3.java
@@ -3,11 +3,14 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
 public class CheesePlease3 extends Game {
     public Stage mainStage;
@@ -15,6 +18,7 @@ public class CheesePlease3 extends Game {
     private BaseActor cheese;
     private BaseActor floor;
     private BaseActor winText;
+    private boolean win;
 
     public void create() {
         mainStage = new Stage();
@@ -31,12 +35,14 @@ public class CheesePlease3 extends Game {
         mousey = new BaseActor();
         mousey.setTexture(new
                 Texture(Gdx.files.internal("mouse.png")));
+        mousey.setOrigin( mousey.getWidth()/2, mousey.getHeight()/2
+        );
         mousey.setPosition(20, 20);
         mainStage.addActor(mousey);
         winText = new BaseActor();
         winText.setTexture(new
                 Texture(Gdx.files.internal("you-win.png")));
-        winText.setPosition(170, 60);
+        winText.setPosition(170, 160);
         winText.setVisible(false);
         mainStage.addActor(winText);
     }
@@ -46,13 +52,13 @@ public class CheesePlease3 extends Game {
         mousey.velocityX = 0;
         mousey.velocityY = 0;
         if (Gdx.input.isKeyPressed(Keys.LEFT))
-            mousey.velocityX -= 200;
+            mousey.velocityX -= 150;
         if (Gdx.input.isKeyPressed(Keys.RIGHT))
-            mousey.velocityX += 200;
+            mousey.velocityX += 150;
         if (Gdx.input.isKeyPressed(Keys.UP))
-            mousey.velocityY += 200;
+            mousey.velocityY += 150;
         if (Gdx.input.isKeyPressed(Keys.DOWN))
-            mousey.velocityY -= 200;
+            mousey.velocityY -= 150;
 // update
         float dt = Gdx.graphics.getDeltaTime();
         mainStage.act(dt);
@@ -61,8 +67,32 @@ public class CheesePlease3 extends Game {
                 = cheese.getBoundingRectangle();
         Rectangle mouseyRectangle
                 = mousey.getBoundingRectangle();
-        if (cheeseRectangle.contains(mouseyRectangle))
-            winText.setVisible(true);
+        if ( !win && cheeseRectangle.contains( mouseyRectangle ) )
+        {
+            win = true;
+            Action spinShrinkFadeOut = Actions.parallel(
+                    Actions.alpha(1), // set transparency value
+                    Actions.rotateBy(360, 1), // rotation amount, duration
+                    Actions.scaleTo(0, 0, 1), // x amount, y amount, duration
+                    Actions.fadeOut(1) // duration of fade out
+            );
+            cheese.addAction( spinShrinkFadeOut );
+            Action fadeInColorCycleForever = Actions.sequence(
+                    Actions.alpha(0), // set transparency value
+                    Actions.show(), // set visible to true
+                    Actions.fadeIn(2), // duration of fade in
+                    Actions.forever(
+                            Actions.sequence(
+// color shade to approach, duration
+                                    Actions.color( new Color(1,0,0,1), 1 ),
+                                    Actions.color( new Color(0,0,1,1), 1 )
+                            )
+                    )
+            );
+            winText.addAction( fadeInColorCycleForever );
+        }
+//        if (cheeseRectangle.contains(mouseyRectangle))
+//            winText.setVisible(true);
 // draw graphics
         Gdx.gl.glClearColor(0.8f, 0.8f, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
